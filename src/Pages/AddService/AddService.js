db7c174ddf04199daa345194588559b2ddc10229
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-03 02:20:08 +0600

    add service is done

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,19 +3,42 @@ import { useForm } from "react-hook-form";
 
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/addservice`;
+        fetch(url, {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(result => {
+                if(result.acknowledged===true){
+                    alert('your service is successfully added');
+                    reset();
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+
+    }
 
 
     return (
         <div className='w-50 mx-auto'>
             <h1>add service this page </h1>
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                <input className='mb-2' placeholder='Name' {...register("Name", { required: true, maxLength: 20 })} />
-                <textarea className='mb-2' placeholder='Descripion' {...register("Descripion")} />
+                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea className='mb-2' placeholder='Descripion' {...register("description")} />
                 <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
                 <input className='mb-2' placeholder='Photo Url' type="text" {...register("img")} />
-                <input className='btn btn-primary' type="submit" />
+                <input className='btn btn-primary' type="submit" value='Add Service ' />
             </form>
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -26,8 +26,8 @@ const Header = () => {
                             <Nav.Link href="home">Home</Nav.Link>
                             <Nav.Link href="home#services">Services</Nav.Link>
                             <Nav.Link href="home#experts">Experts</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown title="More" id="collasible-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="addservice">Add Service</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
