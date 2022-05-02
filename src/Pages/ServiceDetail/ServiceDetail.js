227d6c9741e76a64fd6a42716cd7f33ede30bb1d
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-03 00:31:44 +0600

    service details add with data fetch for api and showind all details in api

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/service/${serviceId}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data));
+    })
     return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
+        <div className='d-flex flex-column flex-md-row w-100 w-md-75 m-auto p-3'>
+            <div className='p-3 w-100 w-md-25 d-flex content-center justify-content-md-end'>
+                <img src={service.img} alt="" />
+            </div>
+            <div className='p-3 w-100 d-flex justify-content-md-start'>
+                <div>
+                    <h2>Welcome to Detail: <small className='text-primary'> {service.name}</small></h2>
+                    <h3>Price: <small className='text-primary'> {service.price}</small></h3>
+                    <p>{service.description}</p>
+                    <div className=''>
+                        <Link to="/checkout">
+                            <button className='btn btn-primary'>Proceed Checkout</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
