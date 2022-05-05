f23ff9431a63265126683de44624e48c0e1fdff9
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-05 21:08:03 +0600

    authuration code add in orders page

--- src/Pages/Checkout/Orders/Oders.js
+++ src/Pages/Checkout/Orders/Oders.js
@@ -1,18 +1,34 @@
 import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const Orders = () => {
+    const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
     useEffect(() => {
 
         const getorders = async () => {
-            const url = 'http://localhost:5000/orders';
-            const { data } = await axios.get(url);
-            setOrders(data);
-        }
+            const email = user.email;
+            const url = `http://localhost:5000/orders?email=${email}`;
+            try{
+                const { data } = await axios.get(url, {
+                    headers:{
+                        authorization:`Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                setOrders(data);
+            }catch(error){
+                console.log(error.message);
+                if(error.response?.status === 401 || error.response?.status ===403){
+                    signOut(auth);
+                    navigator('/login');
+                }
+            }
+            }
         getorders();
         
-    }, []);
+    }, [user]);
 
 
     return (
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -31,19 +32,23 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
         signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accesstoken);
+        navigate(from, { replace: true });
+
     }
 
     const navigateRegister = event => {
@@ -56,7 +61,7 @@ const Login = () => {
             await sendPasswordResetEmail(email);
             toast('Sent email');
         }
-        else{
+        else {
             toast('please enter your email address');
         }
     }
